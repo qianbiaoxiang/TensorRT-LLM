25a804ab2a42a24915a2c1ce6477450e98b8a334
jesonxiang <jesonxiang@tencent.com>
2025-06-26 22:22:02 +0800

    rebase main for support_hunyuan_moe

--- tensorrt_llm/_torch/models/__init__.py
+++ tensorrt_llm/_torch/models/__init__.py
@@ -7,6 +7,7 @@ from .modeling_deepseekv3 import DeepseekV3ForCausalLM
 from .modeling_exaone4 import Exaone4ForCausalLM
 from .modeling_gemma3 import Gemma3ForCausalLM
 from .modeling_gemma3vl import Gemma3VLM
+from .modeling_hunyuan_moe import HunYuanMoEV1ForCausalLM
 from .modeling_gpt_oss import GptOssForCausalLM
 from .modeling_hyperclovax import HCXVisionForCausalLM
 from .modeling_llama import LlamaForCausalLM
@@ -38,6 +39,8 @@ __all__ = [
     "Gemma3ForCausalLM",
     "Gemma3VLM",
     "HCXVisionForCausalLM",
+    "HunYuanMoEV1ForCausalLM",
+    "Gemma3Model",
     "LlamaForCausalLM",
     "LlavaNextModel",
     "Mistral3VLM",
